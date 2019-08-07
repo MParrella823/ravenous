494cb5b3fbcf09f2dbb9693532c6f23ee534e474
mparrella823 <mparrella823@gmail.com>
2019-08-06 18:46:52 -0600

    building out 'business' component

--- src/components/business/Business.js
+++ src/components/business/Business.js
@@ -1,5 +1,43 @@
 // Business.js
 // Created by Matt Parrella on 8/6/2019
 //
-// This class will handle how a 'Business' component will be formatted and styled
+// This className will handle how a 'Business' component will be formatted and styled
 
+import React from 'react';
+
+
+let business = {
+    imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+className Business extends React.Component {
+    render() {
+        return <div className="Business">
+        <div className="image-container">
+          <img src='https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg' alt=''/>
+        </div>
+        <h2>{business.name}</h2>
+        <div className="Business-information">
+          <div className="Business-address">
+            <p>{business.address}</p>
+            <p>{business.city}</p>
+            <p>{business.state} {business.zipCode}</p>
+          </div>
+          <div className="Business-reviews">
+            <h3>{business.category}</h3>
+            <h3 className="rating">{business.rating}</h3>
+            <p>{reviewCount} reviews</p>
+          </div>
+        </div>
+      </div>
+    }
+
+}
